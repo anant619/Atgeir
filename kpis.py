bb62ed3d70cf2d0606e5fa289714c266edbc3ed4
anant619 <100332241+anant619@users.noreply.github.com>
2023-01-14 22:20:24 +0530

    Update kpis.py

--- kpis.py
+++ kpis.py
@@ -172,7 +172,7 @@ for file in s3_bucket.objects.all():
   try:
       data = json.loads(body)
       table_data = create_json(data)
-      with driver.session(database=database) as session:
+      with driver.session(database="system") as session:
           session.execute_write(g.create_graph, table_data)
   except ValueError as e:
       print ("Json is not valid")
