b3faf644e146415cd05117dc09eaf8d65df7a39d
anant619 <100332241+anant619@users.noreply.github.com>
2023-01-15 12:15:58 +0530

    Update Export_KPIs.py

--- Export_KPIs.py
+++ Export_KPIs.py
@@ -74,26 +74,26 @@ for file in s3_bucket.objects.all():
     new_dict = json.loads(config)
     JSONDict = dict((k.upper().strip(), v.upper().strip()) for k, v in new_dict.items())
  
-    sf_account = JSONDict.get('ACCOUNT')
-    sf_role = JSONDict.get('ROLE')
-    #sf_user = JSONDict.get('NAME')
-    sf_user = 'sayali'
-    #sf_password = JSONDict.get('SNOWFLAKE', 'sf_password')
-    sf_warehouse = JSONDict.get('WAREHOUSE')
-    sf_database = JSONDict.get('DATABASE')
-    sf_schema = JSONDict.get('SCHEMA')
-    sf_password = 'Atgeir@03'
-    #sf_table = JSONDict.get('SNOWFLAKE', 'sf_table')
-    sf_table = 'METADATA_REPORT'
+#     sf_account = JSONDict.get('ACCOUNT')
+#     sf_role = JSONDict.get('ROLE')
+#     #sf_user = JSONDict.get('NAME')
+#     sf_user = 'sayali'
+#     #sf_password = JSONDict.get('SNOWFLAKE', 'sf_password')
+#     sf_warehouse = JSONDict.get('WAREHOUSE')
+#     sf_database = JSONDict.get('DATABASE')
+#     sf_schema = JSONDict.get('SCHEMA')
+#     sf_password = 'Atgeir@03'
+#     #sf_table = JSONDict.get('SNOWFLAKE', 'sf_table')
+#     sf_table = 'METADATA_REPORT'
     
-    #sf_account = 'AFA78268'
-    #sf_role = 'ACCOUNTADMIN'
-    #sf_user = 'sayali'
-    #sf_password = 'Atgeir@03'
-    #sf_warehouse = 'HAWKEYE_WH'
-    #sf_database = 'DATAGEIR_HAWKEYE_DEV'
-    #sf_schema = 'HAWKEYE_APP'
-    #sf_table = 'METADATA_REPORT'
+    sf_account = 'AFA78268'
+    sf_role = 'ACCOUNTADMIN'
+    sf_user = 'sayali'
+    sf_password = 'Atgeir@03'
+    sf_warehouse = 'HAWKEYE_WH'
+    sf_database = 'DATAGEIR_HAWKEYE_DEV'
+    sf_schema = 'HAWKEYE_APP'
+    sf_table = 'METADATA_REPORT'
 
 def load_df_to_snowflake(snow, csv_df, dbname, schemaname, tablename):
     # try:
