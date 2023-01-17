037274e63ed66b5390be2680142fda4a44adfc0a
anant619 <100332241+anant619@users.noreply.github.com>
2023-01-17 15:13:32 +0530

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#cd /CMS/CostNomics/costnomics-backend/scripts
+
+input_args=$#
+
+job_type=${1}
+client=${2}
+
+export client_id=${client}
+export config_dir=./config.properties
+
+echo "Executing" ${job_type} "job for client" ${client_id}
+
+if [ $input_args -eq 3 ]
+then
+
+  job_param=${3}
+
+  python3 ${job_param}.py
+  error_code=$?
+
+  if [ $error_code != 0 ]
+  then
+    echo "${job_param}.py  has failed.."
+  else
+    echo "${job_param}.py Completed.."
+  fi
+else
+    printf "%b" "Argument count incorrect. Stopping processing...\n"
+fi
