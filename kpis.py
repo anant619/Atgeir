e991605bfe69b9499c7133069c03e91c6de148b6
anant619 <100332241+anant619@users.noreply.github.com>
2023-01-18 18:24:02 +0530

    Update kpis.py

--- kpis.py
+++ kpis.py
@@ -236,7 +236,8 @@ for file in s3_bucket.objects.all():
 
           uniqueusercount = table_data.get('datasetUsage').get('uniqueUserCount')
           totalSqlQueriesCount = table_data.get('datasetUsage').get('totalSqlQueriesCount')
-          load_timestamp = pd.datetime.now()
+          load_timestamp = numpy.datetime.now()
+          
           print(load_timestamp)
 #           timestamp = table_data.get('timestamp')
 #           timestamp = datetime.fromtimestamp(int(timestamp))
