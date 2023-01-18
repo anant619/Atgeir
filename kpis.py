0964e41fa45c2f19e55ebcad0f5f84a10bc74ab0
anant619 <100332241+anant619@users.noreply.github.com>
2023-01-18 18:12:32 +0530

    Update kpis.py

--- kpis.py
+++ kpis.py
@@ -243,7 +243,7 @@ for file in s3_bucket.objects.all():
          
           columns = ["SOURCE", "DATABASE_NAME", "SCHEMA_NAME", "TABLE_NAME", "TAGS", "UNIQUEUSERUSAGECOUNT","TOTALQUERIESCOUNT","RUNID","FIELDDETAILS","ACTION"]
           data = [source, Database_name,Schema_name,Table_name,tags,uniqueUserCount,totalSqlQueriesCount,RunID,fielddetails,action]
-          df = pd.DataFrame(data)
+          df = pd.DataFrame(data,axis=1)
           df.columns = map(lambda x: str(x), columns)
           print(df)
           snow = utils.snow_connect('AFA78268', 'sayali', 'Atgeir@03', 'ACCOUNTADMIN', 'HAWKEYE_WH', 'DATAGEIR_HAWKEYE_DEV', 'HAWKEYE_APP')
